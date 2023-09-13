70960394a7c8b4582bdc70371876df7560abd983
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2023-09-13 20:42:35 +0530

    configure the department service

--- department-service/src/main/java/com/practice/deparmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/practice/deparmentservice/DepartmentServiceApplication.java
@@ -2,12 +2,14 @@ package com.practice.deparmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class DeparmentServiceApplication {
+@EnableDiscoveryClient
+public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeparmentServiceApplication.class, args);
+		SpringApplication.run(DepartmentServiceApplication.class, args);
 	}
 
 }
